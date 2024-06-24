bf75d4537375d19802c62128856798c0165e7155
fazlyedgenroots <fazly@edgenroots.com>
2024-06-25 03:15:29 +0600

    homepage , header, footer template added

--- src/app/page.js
+++
@@ -1,9 +0,0 @@
-import Image from "next/image";
-import { getAllCourse } from "../../queries/course-queries";
-
-export default async function Home() {
-  const allCourse = await getAllCourse();
-  return (
-    <p>{allCourse.length}</p>
-  );
-}
